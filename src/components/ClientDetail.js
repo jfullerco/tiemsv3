3fdc59a37d5d4c1e43eccb643c1a46186e137253
jfullerco <jonathan@jfuller.co>
2021-01-25 01:50:56 -0500

    1 file modified, 2 added, 1 deleted

--- src/components/ClientDetail.js
+++ src/components/ClientDetail.js
@@ -1,60 +1,18 @@
 import React, {useState, useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import RestDBService from '../services/RestDBService'
-import {Link} from 'react-router-dom'
-import {useSites} from '../hooks/useSites'
-import AssetCount from '../modules/AssetCount'
-import OrderList from '../modules/OrderList'
-import SiteList from '../modules/SiteList'
+import {Link, Route, useParams} from 'react-router-dom'
+import {useClient} from '../hooks/useClient'
 
-const ClientDetail = (props) => {
+function ClientDetail() {
 
-  const {id} = useParams()
-  const [client, setClient] = useState([])
-  const [sites, setSites] = useSites([])
-  const [assets, setAssets] = useState([])
+const {id} = useParams()
+const {client} = useClient()
+useEffect(() => {
   
-  
-  useEffect(() => {
-    retrieveClient(id)
-    }, []
-  )
+}), []
 
-  const retrieveClient = (id) => {
-        RestDBService.getClient(id)
-        .then(res => {
-          setClient(res.data)
-          setSites(res.data.sites)
-          setAssets(res.data.assets)
-        })
-        .catch(e => {
-          console.log(e)
-        })
-      }
-  
-  console.log()
-
-  return (
-    <div>
-      
-      <div className="sub-nav"> 
-        <Link to={`/addSite/${id}`}>Add Site</Link>
-        <Link to={`/addAsset/${id}`}>Add Asset</Link>
-        <Link to={`/admin/client/${id}`}>Update Client</Link>
-      </div>
-
-      <h5>{client.client_name}</h5>
-
-      <div className="container">
-        
-        <AssetCount style="order: 2" assets={assets} />
-        <OrderList style="order: 3" />
-      </div>
-
-      <p />
-       
-            
-    </div>
-  )
+console.log(client)
+return (
+  <div>test</div>
+)
 }
 export default ClientDetail
---
+++ src/hooks/useClient.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import {Link, Route, useParams} from 'react-router-dom'
+import RestDBService from '../services/RestDBService'
+
+export function useClient() {
+  const {id} = useParams()
+  const [ client, setClient ] = useState()
+  const [ sites, setSites ] = useState()
+  const [ assets, setAssets ] = useState()
+  
+  useEffect(() => {
+    retrieveClient(id)
+    }, []
+  )
+
+  const retrieveClient = (id) => {
+        RestDBService.getClient(id)
+        .then(res => {
+          setClient(res.data)
+          setSites(res.data.sites)
+          setAssets(res.data.assets)
+        })
+        .catch(e => {
+          console.log()
+        })
+      }
+
+    return (
+      {client, sites, assets}
+    )
+}
+
