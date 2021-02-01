8948386fdf4f5b516fbe8de4d26c1f4a8d551f59
jfullerco <jonathan@jfuller.co>
2021-02-01 13:13:50 -0500

    1 file modified

--- src/hooks/useClient.js
+++ src/hooks/useClient.js
@@ -5,28 +5,28 @@ import RestDBService from '../services/RestDBService'
 export function useClient() {
   const {id} = useParams()
   const [ client, setClient ] = useState([])
-  const [ sites, setSites ] = useState([])
+  const [ sites, setSites ] = useState()
   const [ assets, setAssets ] = useState([])
   
   useEffect(() => {
     retrieveClient(id)
     }, []
   )
-
+console.log(sites)
   const retrieveClient = (id) => {
-        RestDBService.getClient(id)
+        RestDBService.getAllSites(id)
         .then(res => {
           setClient(res.data)
-          setSites(res.data.sites)
-          setAssets(res.data.assets)
+          setSites(res.data)
+          console.log(res.data)
         })
         .catch(e => {
-          console.log()
+          
         })
       }
 
     return (
-      {client, sites, assets}
+      {client, sites}
     )
 }
 
